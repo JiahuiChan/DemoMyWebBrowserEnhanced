daa48dac28d4dc95a632f824c2cb9cadaed59782
17034021 <17034021@rp.edu.sg>
2019-07-30 11:55:38 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demomywebbrowserenhanced/MainActivity.java
@@ -0,0 +1,39 @@
+package sg.edu.rp.c346.demomywebbrowserenhanced;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLoadURL;
+    WebView wvMyPage;
+    EditText etURL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wvMyPage = findViewById(R.id.webViewMyPage);
+
+        wvMyPage.setWebViewClient(new WebViewClient());
+
+        btnLoadURL = findViewById(R.id.buttonLoad);
+        etURL = findViewById(R.id.editTextURL);
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                   String url = "https://www.grab.com/sg/";
+                   wvMyPage.loadUrl(url);
+                   etURL.setVisibility(View.GONE);
+
+            }
+        });
+    }
+}
